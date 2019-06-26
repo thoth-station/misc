43176e63b5feeac0813de46c29f3d5b4746f9f93
Fridolin Pokorny <fridolin@redhat.com>
2019-06-26 17:41:05 +0200

    Fix reference to JSON file to be used

--- schedule_inspection.sh
+++ schedule_inspection.sh
@@ -2,5 +2,5 @@
 
 # Schedule inspection on PSI stage environment multiple times.
 for i in {0..300}; do
-  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d @d.json 'http://amun-api-thoth-amun-api-stage.cloud.paas.psi.redhat.com/api/v1/inspect'
+  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d @inspection.json 'http://amun-api-thoth-amun-api-stage.cloud.paas.psi.redhat.com/api/v1/inspect'
 done
