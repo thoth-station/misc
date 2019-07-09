500469224188716c56ce326a398b6ece214b01f8
Francesco Murdaca <fmurdaca@redhat.com>
2019-07-09 11:52:53 +0200

    add native packages paramerts for base image
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- schedule_performance_benchmarks.py
+++ schedule_performance_benchmarks.py
@@ -63,13 +63,18 @@ def verify_framework_version_installed(framework_name: str, path: str, index_url
 
 
 def create_amun_api_input(
-    image: str, framework: str, framework_version: str, index_url: str, benchmark: str
+    base_image: str, native_packages: list, framework: str, framework_version: str, index_url: str, benchmark: str
 ) -> dict:
     """Create specification for Amun API input."""
     with open("./inspection.json") as json_file:
         specification = json.load(json_file)
 
-    specification["base"] = image
+    specification["base"] = base_image
+    if native_packages:
+        native_packages = native_packages.split(",")
+    else:
+        native_packages = []
+    specification["packages"] = native_packages
     create_pipfile_and_pipfile_lock_inputs(
         framework=framework, framework_version=framework_version, index_url=index_url
     )
@@ -189,19 +194,22 @@ def schedule_performance_benchmarks(
     framework: str,
     framework_version: str,
     benchmark: str,
-    image: str,
+    base_image: str,
+    native_packages: list,
     index_url: str,
     count: int,
 ):
     """Schedule Performance benchmark."""
     verify_script_framework_compatibility(framework=framework, script=benchmark)
-    _LOGGER.info(f"Platform/Base Image selected is {image}")
+    _LOGGER.info(f"Platform/Base Image selected is {base_image}")
+    _LOGGER.info(f"Native packages to be installed on base image: {native_packages}")
     _LOGGER.info(f"Framework/Version selected is: {framework}:{framework_version}")
     _LOGGER.info(f"Index source is: {index_url}")
     _LOGGER.info(f"Performance test selected is: {benchmark}")
     _LOGGER.info(f"Number of inspections requested is: {count}")
     specification = create_amun_api_input(
-        image=image,
+        base_image=base_image,
+        native_packages=native_packages,
         framework=framework,
         framework_version=framework_version,
         index_url=index_url,
@@ -238,11 +246,20 @@ def schedule_performance_benchmarks(
     help="Amun API URL to talk to.",
 )
 @click.option(
-    "--image",
-    "-i",
+    "--base-image",
+    "-bi",
     required=True,
     type=str,
-    help="Platform/ image used to run the inspection.",
+    help="Platform/Base_image used to run the inspection.",
+)
+
+@click.option(
+    "--native-packages",
+    "-np",
+    type=str,
+    default="",
+    show_default=True,
+    help="List of native packages (RPM or Deb packages) that should be installed into the requested base image.",
 )
 @click.option(
     "--framework",
@@ -285,14 +302,16 @@ def cli(
     framework: str,
     framework_version: str,
     benchmark: str,
-    image: str,
+    base_image: str,
+    native_packages: list,
     index_url: str,
     count: int,
 ):
-    """Trigger analysis of inspections for the selected platform/image, index_url and framework."""
+    """Trigger analysis of inspections for the selected platform/base_image, index_url and framework."""
     schedule_performance_benchmarks(
         amun_api_url=amun_api_url,
-        image=image,
+        base_image=base_image,
+        native_packages=native_packages,
         framework=framework,
         framework_version=framework_version,
         benchmark=benchmark,
